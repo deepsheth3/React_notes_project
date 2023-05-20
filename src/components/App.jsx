3c48c77f079603374ab11202385380f10aca31dc
deep sheth <deepsheth3@gmail.com>
2023-05-20 13:31:48 +0530

    Added reacts hooks for dymanic note addition and deletion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes"
 
-function entryNote(singleNote){
-    return(
-        <Note
-            key = {singleNote.key}
-            title = {singleNote.title}
-            content = {singleNote.content}
-        />
-    )
-}
+import CreateArea from "./createArea";
+
 
 function App(){
+    
+    const [notes,setNotes] = useState([])
+
+    function addNote(note){
+        setNotes(prevValue => {
+            return [
+                ...prevValue,
+                note
+            ]
+        })
+    }
+
+    function deleteNote(id){
+        setNotes(prevValue=>{
+            return prevValue.filter((noteItem,index)=>{ return index != id}) 
+        })
+    }
+
     return(
-        
         <div>
             <Heading/>
-            {notes.map(entryNote)}
+            <CreateArea onAdd={addNote}/>
+            {notes.map((singleNote,index)=>(<Note key = {index} id={index} title = {singleNote.title} content = {singleNote.content} onDelete={deleteNote}/>))}
             <Footer/>
         </div>
-
     )
 }
     
--- src/notes.js
+++ src/notes.js
@@ -22,7 +22,13 @@ const notes = [
       title: "Hardware vs. Software",
       content:
         "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
-    }
+    },
+    {
+        key: 5,
+        title: "What is your fav food?",
+        content:
+          "Easy answer! Pasta!!"
+      }
   ];
 
 export default notes
